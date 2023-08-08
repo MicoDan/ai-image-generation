aac62ac31ccf8065b2a31207b9b291daba95ddb1
MicoDan <121478029+MicoDan@users.noreply.github.com>
2023-08-08 09:02:34 +0200

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/v1/post', {
+      const response = await fetch('https://ai-image-generation-two.vercel.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
